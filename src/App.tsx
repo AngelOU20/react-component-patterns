4a1c144ad61f02a28202043c01c58b3ebd30892a
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-11 16:16:02 -0500

    feat: implement Loader component in the fallback

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Suspense } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AppRouter } from './router/AppRouter';
+import { Loader } from './components/Loader';
 
-function App (): JSX.Element {
+function App(): JSX.Element {
   return (
-    <Suspense fallback={<span>Loading ...</span>}>
+    <Suspense fallback={<Loader />}>
       <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
