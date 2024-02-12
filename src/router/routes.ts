dbbae0ebb8c3120a5bd57aa4d41da898e6e71eaf
Julio Ucharima <julio_ucharima@usmp.pe>
2024-02-12 15:50:02 -0500

    feat: update routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,20 +6,16 @@ type JSXComponent = () => JSX.Element;
 interface Route {
   to: string;
   path: string;
-  // Component: React.FC;
   Component: LazyExoticComponent<JSXComponent> | React.FC | JSXComponent;
   name: string;
 }
 
 /* LazyLoad */
 const lazyLoad = lazy(() => import("../01-lazyload/pages/LazyPage1"));
-const compoundComponent = lazy(
-  () => import("../02-compound-components/pages/ShoppingPage")
-);
-const extensibleStyles = lazy(
-  () => import("../03-extensible-styles/pages/ShoppingPage")
-);
+const compoundComponent = lazy(() => import("../02-compound-components/pages/ShoppingPage"));
+const extensibleStyles = lazy(() => import("../03-extensible-styles/pages/ShoppingPage"));
 const controlProps = lazy(() => import("../04-control-props/pages/ShoppingPage"));
+const stateInitializer = lazy(() => import("../05-state-initializer/pages/ShoppingPage"));
 
 export const routes: Route[] = [
   {
@@ -41,9 +37,15 @@ export const routes: Route[] = [
     name: "Extensible styles",
   },
   {
-    to: "/control-Props",
-    path: "control-Props",
+    to: "/control-props",
+    path: "control-props",
     Component: controlProps,
     name: "Control props",
+  },
+  {
+    to: "/state-initializer",
+    path: "state-initializer",
+    Component: stateInitializer,
+    name: "State initializer",
   },
 ];
